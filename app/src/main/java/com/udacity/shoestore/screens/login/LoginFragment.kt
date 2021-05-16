a89e2124ed18f452f2f248d2a982caca2f984411
Vlad Spreys <vlad@spreys.com>
2021-05-16 11:54:04 +1000

    feat: Added login fragment

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -1,6 +1,29 @@
 package com.udacity.shoestore.screens.login
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.LoginFragmentBinding
 
 class LoginFragment : Fragment() {
+    private lateinit var binding: LoginFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.login_fragment,
+            container,
+            false
+        )
+
+        return binding.root
+    }
 }
