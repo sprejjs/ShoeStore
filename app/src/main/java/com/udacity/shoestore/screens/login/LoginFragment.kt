0827ecbd0c42fd66ba8247e75a2927b8984fa9a2
Vlad Spreys <vlad@spreys.com>
2021-05-23 12:01:19 +1000

    feat: Added the Welcome fragment

--- app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.LoginFragmentBinding
 
@@ -23,6 +24,14 @@ class LoginFragment : Fragment() {
             container,
             false
         )
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
+
+        binding.signUpButton.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToWelcomeFragment())
+        }
 
         return binding.root
     }
